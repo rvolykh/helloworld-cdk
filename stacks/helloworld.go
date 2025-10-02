e9c05906b9549d517d28002e8f65e3fbc65d7a59
Roman Volykh <rvolykh@gmail.com>
2025-10-02 17:51:08 +0300

    Setup new cdk project and change structure a bit

---
+++ stacks/helloworld.go
@@ -0,0 +1,32 @@
+package stacks
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+)
+
+type HelloworldCdkStackProps struct {
+	awscdk.StackProps
+}
+
+func NewHelloworldCdkStack(scope constructs.Construct, id string, props *HelloworldCdkStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	// The code that defines your stack goes here
+
+	// example resource
+	queue := awssqs.NewQueue(stack, jsii.String("HelloworldCdkQueue.fifo"), &awssqs.QueueProps{
+		VisibilityTimeout:         awscdk.Duration_Seconds(jsii.Number(300)),
+		Fifo:                      jsii.Bool(true),
+		ContentBasedDeduplication: jsii.Bool(true),
+	})
+	_ = queue
+
+	return stack
+}
